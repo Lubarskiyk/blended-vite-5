4ae0675b67a55c19947131480c194d8b325c98c9
Olexandr-L45D <litvinenko1978aleks@gmail.com>
2024-10-06 17:08:04 +0300

    upd

--- src/components/ExchangeForm/ExchangeForm.jsx
+++ src/components/ExchangeForm/ExchangeForm.jsx
@@ -1,14 +1,36 @@
 import { RiExchangeDollarFill } from 'react-icons/ri';
 import styles from './ExchangeForm.module.css';
 
+const Formula = /^\d+(\.\d{1,2})?\s[a-zA-Z]{3}\sin\s[a-zA-Z]{3}$/
+// {
+//   to: 'UAH',
+//     from: 'USD',
+//       amount: 15
+// }
+
 export const ExchangeForm = () => {
+  const hundleSubmit = (ev) => {
+    ev.preventDefault();
+    const request = ev.target.elements.request.value;
+    // console.log(request);
+    const isValid = Formula.test(request);
+    // console.log(isValid);
+    if (!isValid) {
+      return
+    }
+    const [amount, from, , to] = request.split(' ');
+    const params = { amount, from, to }
+    console.log(params);
+
+    ev.target.reset()
+  }
   return (
-    <form className={styles.form}>
+    <form onSubmit={hundleSubmit} className={styles.form}>
       <button className={styles.button} type="submit">
         <RiExchangeDollarFill className={styles.icon} />
       </button>
 
-      <input title="Request format 15 USD in UAH" className={styles.input} />
+      <input name="request" title="Request format 15 USD in UAH" className={styles.input} />
     </form>
   );
 };
